4c3c86b44590a62e4ff9709689692434a8f80f47
Marcelo Santos <rocha.mer21@gmail.com>
2021-12-19 14:52:14 -0300

    movido pages para src

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Vendas App</title>
+        <meta name="description" content="Generated br create next app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
+
+export default Home
