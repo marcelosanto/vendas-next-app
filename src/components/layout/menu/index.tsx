faf70ead5bda8bfd7465ecec7b52b2e0d954cc2b
marcelosanto <rocha.mer21@gmail.com>
2021-12-22 19:14:11 -0300

    add menuItem component

--- src/components/layout/menu/index.tsx
+++ src/components/layout/menu/index.tsx
@@ -1,16 +1,33 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 
 export const Menu: NextPage = () => {
   return (
     <aside className="column is-2 is-narrow-mobile is-fullheight section is-hidden-mobile">
       <p className="menu-label is-hidden-touch">Minhas vendas</p>
       <ul className="menu-list">
-        <li>
-          <a href="#">
-            <span className="icon"></span>Home
-          </a>
-        </li>
+        <MenuItem href="#" label="Home" />
+        <MenuItem href="#" label="Cadastro" />
+        <MenuItem href="#" label="Configurações" />
+        <MenuItem href="#" label="Sair" />
       </ul>
     </aside>
+  )
+}
+
+interface MenuItemProps {
+  href: string
+  label: string
+}
+
+const MenuItem: NextPage<MenuItemProps> = ({ href, label }: MenuItemProps) => {
+  return (
+    <li>
+      <Link href={href}>
+        <a>
+          <span className="icon"></span> {label}
+        </a>
+      </Link>
+    </li>
   )
 }
