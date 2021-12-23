2f4a1cc4d430c40475ef958fbab6391dc25806b8
Marcelo Santos <rocha.mer21@gmail.com>
2021-12-23 10:12:54 -0300

    fix: aliamento dos formularios

--- src/components/produtos/cadastro/index.tsx
+++ src/components/produtos/cadastro/index.tsx
@@ -5,55 +5,61 @@ import { Layout } from 'components'
 export const Cadastro: NextPage = () => {
   return (
     <Layout titulo="Produtos">
-      <div className="field">
-        <label className="label" htmlFor="inputSku">
-          SKU: *
-        </label>
-        <div className="control">
-          <input
-            id="inputSku"
-            type="text"
-            className="input"
-            placeholder="Digite o SKU do produto"
-          />
+      <div className="columns">
+        <div className="column  field">
+          <label className="label" htmlFor="inputSku">
+            SKU: *
+          </label>
+          <div className="control">
+            <input
+              id="inputSku"
+              type="text"
+              className="input"
+              placeholder="Digite o SKU do produto"
+            />
+          </div>
+        </div>
+        <div className="column field">
+          <label className="label" htmlFor="inputPreco">
+            Preço: *
+          </label>
+          <div className="control">
+            <input
+              id="inputPreco"
+              type="text"
+              className="input"
+              placeholder="Digite o Preço do produto"
+            />
+          </div>
         </div>
       </div>
-      <div className="field">
-        <label className="label" htmlFor="inputPreco">
-          Preço: *
-        </label>
-        <div className="control">
-          <input
-            id="inputPreco"
-            type="text"
-            className="input"
-            placeholder="Digite o Preço do produto"
-          />
+      <div className="columns">
+        <div className="field column">
+          <label className="label" htmlFor="inputNome">
+            Nome: *
+          </label>
+          <div className="control">
+            <input
+              id="inputNome"
+              type="text"
+              className="input"
+              placeholder="Digite o Nome do produto"
+            />
+          </div>
         </div>
       </div>
-      <div className="field">
-        <label className="label" htmlFor="inputNome">
-          Nome: *
-        </label>
-        <div className="control">
-          <input
-            id="inputNome"
-            type="text"
-            className="input"
-            placeholder="Digite o Nome do produto"
-          />
-        </div>
-      </div>
-      <div className="field">
-        <label htmlFor="textareaDescricao" className="label">
-          Descrição: *
-        </label>
-        <div className="control">
-          <textarea
-            id="textareaDescricao"
-            className="textarea"
-            placeholder="Digite a Descrição detalhada do produto"
-          />
+      <div className="columns">
+        <div className="field column">
+          <label htmlFor="textareaDescricao" className="label">
+            Descrição: *
+          </label>
+          <div className="control">
+            <textarea
+              id="textareaDescricao"
+              className="textarea"
+              placeholder="Digite a Descrição detalhada do produto"
+            />
+          </div>
         </div>
       </div>
       <div className="field is-grouped">
